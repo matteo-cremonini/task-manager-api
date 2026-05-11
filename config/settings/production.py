29ca328c023ba254e8b2a0c785106a728d06bf42
Matteo <mcrema999@outlook.it>
2026-05-11 18:44:46 +0200

    fix: add CSRF_TRUSTED_ORIGINS for browsable API in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,6 +8,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = [
+    f'https://{host}' for host in ALLOWED_HOSTS if host
+]
+
 DATABASES = {
     'default': dj_database_url.config(
         conn_max_age=600,
