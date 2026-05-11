d198a8319eb11d6b55e014515fbf15063538f0ec
Matteo <mcrema999@outlook.it>
2026-05-11 18:50:20 +0200

    fix: display owner username instead of ID in Project and Task serializers

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,22 +4,23 @@ from rest_framework import serializers
 
 class TaskSerializer(serializers.ModelSerializer):
     project_name = serializers.StringRelatedField(source='project')
-    
-    
+    owner = serializers.StringRelatedField()
+
     class Meta:
         model = Task
-        fields = [ 'id', 'title', 'description', 'project', 'status', 'is_working_on','completed', 'project_name', 'owner', 'created_at', 'updated_at']
-        read_only_fields = ['owner','created_at', 'updated_at']
+        fields = ['id', 'title', 'description', 'project', 'status', 'is_working_on', 'completed', 'project_name', 'owner', 'created_at', 'updated_at']
+        read_only_fields = ['owner', 'created_at', 'updated_at']
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     completion_percentage = serializers.SerializerMethodField()
+    owner = serializers.StringRelatedField()
     tasks = TaskSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Project
         fields = ['id', 'name', 'description', 'timeline', 'tasks', 'completion_percentage', 'owner', 'created_at',]
-        read_only_fields = ['owner','created_at',]
+        read_only_fields = ['owner', 'created_at',]
 
     def get_completion_percentage(self, obj):
         return obj.completion_percentage()
